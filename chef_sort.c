8894ddc1a3d637d6755b682d5d3e82405786b60b
Trecto34 <trecto34@protonmail.com>
2026-06-17 08:23:12 -0300

    Nível Novato

--- chef_sort.c
+++ chef_sort.c
@@ -35,7 +35,26 @@ typedef struct {
 // Dica: Use strcmp() da biblioteca <string.h> para comparar strings.
 // Dica: Passe ponteiros para contadores de comparações e trocas se quiser alterá-los dentro da função.
 void bubbleSortStrings(char arr[][50], int n, int *comparacoes, int *trocas) {
-    // Sua lógica do Bubble Sort aqui
+	int i, j, trocou;
+	char temp[50];
+
+	for (i = 0; i<n; i++){
+		trocou = 0;
+		for (j = 0; j<n - i - 1; j++){
+			int comp = strcmp(arr[j], arr[j+1]);
+			(*comparacoes)++;
+			if (comp > 0) {
+				strcpy(temp, arr[j]);
+				strcpy(arr[j], arr[j+1]);
+				strcpy(arr[j+1], temp);
+				trocou = 1;
+				(*trocas)++;
+			}
+
+		}
+		if (!trocou)
+			break;
+	}
 }
 
 // Nível Aventureiro: Selection Sort para array de structs (Prato)
@@ -64,7 +83,6 @@ int main() {
     // ---------------------------------------------------------
     // ÁREA DO NÍVEL NOVATO (Despensa / Bubble Sort)
     // ---------------------------------------------------------
-    /*
     char ingredientes[5][50] = {"Tomate", "Cebola", "Alho", "Cenoura", "Batata"};
     int num_ingredientes = 5;
     int comparacoes = 0;
@@ -74,12 +92,22 @@ int main() {
     printf("Lista ANTES da ordenacao:\n");
     // Laco para imprimir ingredientes antes
 
-    // Chamada da funcao bubbleSortStrings(...)
+    int tamanho = sizeof(ingredientes)/sizeof(ingredientes[0]);
+    for (int i=0; i<tamanho; i++){
+	    printf("%s ",ingredientes[i]);
+    };
+
+	bubbleSortStrings(ingredientes, num_ingredientes, &comparacoes, &trocas);
+
 
     printf("\nLista DEPOIS da ordenacao:\n");
     // Laco para imprimir ingredientes depois
     // Imprimir totais de comparacoes e trocas
-    */
+    //
+    for (int i=0; i<num_ingredientes; i++){
+	    printf("%s ", ingredientes[i]);
+    }
+    printf("Comparações: %d\nTrocas: %d", comparacoes, trocas);
 
 
     // ---------------------------------------------------------
